0cb5b0ed131f5472df1ea551e7b3e44e032caa62
Filon Oikonomou <filon@admin.grnet.gr>
2024-05-29 13:40:50 +0300

    nvim: Disable auto-formatting on save

---
+++ nvim/.config/nvim/lua/plugins/astrolsp.lua
@@ -0,0 +1,9 @@
+return {
+    "AstroNvim/astrolsp",
+    ---@type AstroLSPOpts
+    opts = {
+        formatting = {
+            format_on_save = false, -- enable or disable automatic formatting on save
+        },
+    },
+}
