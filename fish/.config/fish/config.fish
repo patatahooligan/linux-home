c89263537aaf10dde647e01c194df77b34615b1b
patatahooligan <filwn.oikonomou@gmail.com>
2025-04-07 18:51:54 +0300

    fish: Add pyenv

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,4 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_add_path /usr/lib/jvm/default/bin
     source /usr/local/share/shell/settings
+
+    if command -q pyenv
+        pyenv init - fish | source
+    end
 end
