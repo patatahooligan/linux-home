1505d117d4327960c5a1148ec5aba0515951a3f3
patatahooligan <filwn.oikonomou@gmail.com>
2026-01-17 04:47:40 +0200

    fish: Show full pwd in prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -3,7 +3,7 @@ function fish_prompt
         echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
     end
 
-    echo -n (set_color blue)(prompt_pwd)' '
+    echo -n (set_color blue)(prompt_pwd -d 0)' '
 
     set_color -o
     if fish_is_root_user
